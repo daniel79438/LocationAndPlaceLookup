f5a9ea7ab97bb0fc70fee0ad2c13fa89d1e20914
daniel79438 <daniel79438@gmail.com>
2025-04-12 21:15:10 +0100

    Initial Commit

---
+++ LocationAndPlaceLookup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocationAndPlaceLookup
+//
+//  Created by Daniel Harris on 12/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocationAndPlaceLookup/LocationAndPlaceLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAndPlaceLookupApp.swift
+//  LocationAndPlaceLookup
+//
+//  Created by Daniel Harris on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LocationAndPlaceLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
