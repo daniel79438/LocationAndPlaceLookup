02d280c2206026c9dffcbd68b8ba91da9c8c3d15
daniel79438 <daniel79438@gmail.com>
2025-04-12 22:47:59 +0100

    Icons

---
+++ LocationAndPlaceLookup/LocationManager.swift
@@ -0,0 +1,66 @@
+//
+//  LocationManager.swift
+//  LocationAndPlaceLookup
+//
+//  Created by Daniel Harris on 12/04/2025.
+//
+
+import Foundation
+import MapKit
+
+@Observable
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    // *** CRITICALLY IMPORTANT *** Always add info.plist message for Privacy - Location When in Use Usage Description
+    
+    var location: CLLocation?
+    private let locationManager = CLLocationManager()
+    var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    var errorMessage: String?
+    
+    override init() {
+        super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+    
+    
+    
+    // Delegate methods that Apple has created & will call, but that we filled out
+extension LocationManager {
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let newLocation = locations.last else { return } // Use the last location as the location
+            location = newLocation
+            
+            // You can uncomment this when you only want to get the location once, not repeatedly
+            // manager.stopUpdatingLocation()
+        }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+        
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            print("LocationManager authorization granted.")
+            manager.startUpdatingLocation()
+        case .denied, .restricted:
+            print("LocationManager authorization denied.")
+            errorMessage = "😡🛑 LocationManager access denied"
+            manager.stopUpdatingLocation()
+        case .notDetermined:
+            print("LocationManager authorization not determined")
+            manager.requestWhenInUseAuthorization()
+        @unknown default:
+            print("Look for new enum for CLocationManager.authorizationSatuts()")
+            manager.requestAlwaysAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        errorMessage = error.localizedDescription
+        print("😡🗺️ ERROR LocationManager: \(errorMessage ?? "n/a")")
+    }
+}
